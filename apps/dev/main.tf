3f8141cbaafbc0b591f2ce335290279a42b17458
dstolarek01 <144921032+dstolarek01@users.noreply.github.com>
2024-04-10 13:30:42 +0200

    update app dev configuration

--- apps/dev/main.tf
+++ apps/dev/main.tf
@@ -1,29 +1,20 @@
-module "load_balanecr" {
-  source = "../../modules/load_balancer"
-  lb = {
-    name     = "dstolarek-lb"
-    internal = false
-    sg       = "sg-0b6fcdb09f37eb537"       # From the infra/dev result
-    subnets  = ["subnet-0114fcddfcc500f84"] # From the infra/dev result
-  }
+module "server_frontend" {
+  source = "../../modules/server"
 
-  tg = {
-    name = "dstolarek-tg"
-  }
+  config = {
+    name    = "dstolarek-server-frontend"
+    subnets = [""] # frontend public subnets
 
-  vpc_id = "vpc-005d50ddb18cefa89" # Read it from data-source - filter by name
+    load_balancer = {
+      internal        = false
+      security_groups = [""] # security group for fronted
+      vpc_id          = ""   # vpc_id
+    }
 
-  lt = {
-    name        = "dstolarek-launch-template"
-    description = "dstolarek launch template"
-    user_data   = filebase64("./templates/user_data.sh")
-    public      = true
-    subnet_id   = "subnet-0114fcddfcc500f84" # From the infra/dev result
-    sg          = ["sg-0b6fcdb09f37eb537"]   # From the infra/dev result
-  }
-
-  asg = {
-    name    = "my-asg"
-    subnets = ["subnet-0114fcddfcc500f84"] # From the infra/dev result
+    launch_template = {
+      user_data       = filebase64("./templates/frontend.sh") # Mb use template_file?
+      public          = true
+      security_groups = [""] # public ec2 security_group
+    }
   }
 }
